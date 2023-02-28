cef3351bc6eeaf3f9da6f5560791754b72783fc0
tonmoy dey <81032207+TonmoyDey@users.noreply.github.com>
2023-03-01 00:40:23 +0600

    Add files via upload

---
+++ lab4/changeprofilepicturecheck.php
@@ -0,0 +1,31 @@
+<?php
+session_start();
+if(isset($_SESSION['flag'])){
+?>
+
+
+
+<html>
+    <head>
+        <title>Change Profile Picture</title>
+    </head>
+    <body>
+    <center>
+        <table border ="1" height=400 width=800>
+            <tr height=70>
+
+                <td colspan="2"> 
+                    <table width = "800">
+                    <tr>
+                        <td>
+                            <img height="70" src="xcompany.png">
+                        </td>
+                        <td align="right">
+                        Logged in as <a href="viewprofile.php"> <?php echo $_SESSION['username'] ?></a> |
+                                    <a href="logout.php">Logout</a>
+                    </td>    
+                </tr>    
+                </table>
+                </td>
+            </tr>
+</html>
