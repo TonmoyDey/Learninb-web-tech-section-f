3a0ca2ff298e8439fa9ab572df2d883b5017223f
tonmoy dey <81032207+TonmoyDey@users.noreply.github.com>
2023-03-01 00:39:56 +0600

    Add files via upload

---
+++ lab3/current(1,2,3,4,5,6).php
@@ -0,0 +1,104 @@
+<html>
+    <title>
+        <head>
+            LAB TASK 3
+        </head>
+    </title>
+    <body>
+      
+ <fieldset>
+
+        <form method="Post" action="handler.php">
+            <fieldset width ="4">
+                <legend> Register </legend><hr>
+
+           
+         
+            Name
+            <input type="text"name="name"value=""/><hr>
+            <br>
+          
+            Email
+            <input type="text"name="email"value=""/><hr>
+            <br>
+           
+            user_name
+            <input type="date"name="User_name"value=""/><hr>
+            <br>
+
+            <legend>Gender</legend>
+            <input type="radio"name="gender"value="male"/>Male
+            <input type="radio"name="gender"value="female"/>Female
+            <input type="radio"name="gender"value="other"/>Other<hr>
+            <br>
+
+            user_name
+            <input type="date"name="User_name"value=""/><hr>
+            <br>
+
+            Confirm_Password<br>
+            <input type= "text" name="Confirm_Password"value=""/><hr>
+            <br>
+
+            Gender<br>
+            <input type="radio"name="gender"value="male"/>Male
+            <input type="radio"name="gender"value="female"/>Female
+            <input type="radio"name="gender"value="other"/>Other<hr>
+            <br>
+
+            <legend>date_of_birth<br></legend>
+            <input type="date"name="date_of_birth"value=""/>
+            <br>
+
+
+
+        </select>
+        <hr>
+        <br>
+
+            
+
+            Submit
+            <input type="submit" name="" value="submit"/>
+            
+            Reset
+            <input type="reset" name="" value="reset"/>
+</fieldset>
+
+        </form>
+
+    </body>
+    <tr> <td>Xcompany</td><td></td><td> <a href="">Home</a> <a href="">| LOGIN</a> <a href="" ></a>| Register</td></tr>
+        <tr><td></td><td><fieldset width ="4">
+            <h1> Register</h1><hr>
+            Name
+            <input type="text"name="name"value=""/><hr>
+            <br></td><td></td></tr>
+        <tr><td></td><td>Email
+            <input type="text"name="email"value=""/><hr>
+            <br></td><td></td></tr>
+        <tr><td></td><td>user_name
+            <input type="date"name="User_name"value=""/><hr>
+            <br></td><td></td></tr>
+        <tr><td></td><td>  user_name
+            <input type="date"name="User_name"value=""/><hr>
+            <br></td><td></td></tr>
+        <tr><td></td><td> Confirm_Password<br>
+            <input type= "text" name="Confirm_Password"value=""/><hr>
+            <br></td><td></td></tr>
+        <tr><td></td><td>Gender<br>
+            <input type="radio"name="gender"value="male"/>Male
+            <input type="radio"name="gender"value="female"/>Female
+            <input type="radio"name="gender"value="other"/>Other<hr>
+            <br></td><td></td></tr>
+        <tr><td></td><td>date_of_birth<br>
+            <input type= "data"name="date_of_birth"value=""/>
+            <br></td><td></td></tr>
+        <tr> <td>Submit
+            <input type="submit" name="" value="submit"/>
+            
+            Reset
+            <input type="reset" name="" value="reset"/></td><td></td><td></td></tr>
+        </fieldset>
+
+</html>
