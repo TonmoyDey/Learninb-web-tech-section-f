50d28becb0f19219f0f90afbdebda4b4a3719a01
tonmoy dey <81032207+TonmoyDey@users.noreply.github.com>
2023-03-14 23:51:08 +0600

    Add files via upload

---
+++ webtec mid/item_availability.php
@@ -0,0 +1,37 @@
+<html>
+    <body>
+    <form>
+    
+        <table border="1" heigth="500" width="600" >
+
+           
+            <tr>
+                <td> <h3>Serial number</h3></td> <td><h3>Types of books </h3> </td>
+            </tr>
+                
+            <tr>
+                 <td>item 1  </td> 
+                 <td>adventure </td> 
+
+            </tr>
+
+           
+            <tr>
+                 <td>Item 2</td> 
+                 <td>Horror</td>
+            </tr>
+
+
+            <tr>
+                <td>Item3</td><td> Romantic</td>
+            </tr>
+
+
+
+
+        </table>
+
+        <button><a href="homepage.php">BACK </a> </button>
+        </form>
+    </body>
+</html>
---
+++ webtec mid/item_checkin.php
@@ -0,0 +1,11 @@
+<html>
+
+<body>
+   <form>
+        <a href="item_availability.php">All Books</a>
+
+   </form>    
+
+</body>
+
+</html>
