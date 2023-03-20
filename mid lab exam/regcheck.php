9bbd5754ae3c8f7399fbb22d3a5c68ca39095492
tonmoy dey <81032207+TonmoyDey@users.noreply.github.com>
2023-03-20 22:42:34 +0600

    Add files via upload

---
+++ mid lab exam/regcheck.php
@@ -0,0 +1,33 @@
+<?php 
+    session_start();
+    if(isset($_REQUEST['submit'])){
+     
+        $username = $_REQUEST['username']; 
+        $name = $_REQUEST['name'];
+        $email = $_REQUEST['email'];
+        $gender = $_REQUEST['gender']; 
+        $password = $_REQUEST['password']; 
+        $confirmpassword= $_REQUEST['confirmpassword'];
+        $role=$_REQUEST['role'];
+
+        if($username == "" || $password == "" ||empty($name)|| $email == "" || empty($gender)||empty($role)) {
+            echo "null value";
+            
+        }
+        elseif( $password != $confirmpassword  ) {
+            echo "Password not matching";
+            
+        }
+        elseif( strlen(trim($_REQUEST['password'])) < 4) {
+            echo "Password must have at least 5 characters";
+         } else{
+            $file = fopen('index.txt', 'a');
+            $user = $username."|".$password."|".$name."|".$email."|".$gender."|".$role."\r\n";
+            fwrite($file, $user);
+            header('location: login!.php');
+    {
+        echo "User not found";
+    }
+  }
+}
+?>
---
+++ mid lab exam/search Management.php
@@ -0,0 +1,19 @@
+<html>
+
+    <body>
+        
+          <form>
+            <fieldset>
+                <legend>Search Management</legend>
+                <ul>
+                    <li><a href="searchSpecification">Search Specification</a></li>
+                    <li><a href="searchFeature">Search Feature</a></li>
+                    <li><a href="searchApp">Search App</a></li>
+                </ul>
+                
+            </fieldset>
+          </form>
+
+    </body>
+
+</html>
