902054993f853cca9339acceac17d243e5a46955
tonmoy dey <81032207+TonmoyDey@users.noreply.github.com>
2023-03-20 22:49:48 +0600

    Add files via upload

---
+++ mid lab exam/searchSpecification.php
@@ -0,0 +1,15 @@
+<html>
+
+    <body>
+        
+          <form>
+            <fieldset>
+                <legend>Search Specification</legend>
+                     <input type="text" name="search" value="">
+                     <button>search</button>
+            </fieldset>
+          </form>
+
+    </body>
+
+</html>
