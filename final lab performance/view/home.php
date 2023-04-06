fc35464d3de52b351aa7997f3a5af49d053f350b
tonmoy dey <81032207+TonmoyDey@users.noreply.github.com>
2023-04-06 14:24:48 +0600

    Add files via upload

---
+++ final lab performance/view/home.php
@@ -0,0 +1,20 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head> 
+	<form>
+	
+</head>
+
+<body>
+	<h1>Welcome home!</h1>
+	<a href="create.php">Create New Product</a> |
+	<a href="productList.php">Product List </a>
+	</form>
+</body>
+
+</html>
---
+++ final lab performance/view/productList.php
@@ -0,0 +1,43 @@
+<?php
+
+require_once('../model/productsModel.php');
+$result = getAllProducts();
+$count = mysqli_num_rows($result);
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Product List</title>
+</head>
+
+<body>
+
+    <table border="1" align="center">
+        <tr>
+            <th>NAME</th>
+            <th>PROFIT</th>
+            <th>&nbsp;</th>
+        </tr>
+
+        <?php while ($data = mysqli_fetch_assoc($result)) { ?>
+            <tr>
+
+                <td><?= $data['product_name'] ?></td>
+                <td><?= ((int)$data['product_selling_price'] - (int)$data['product_buying_price']); ?></td>
+
+                <td>
+                    <a href="edit.php?id=<?php echo $data['id']; ?>"> EDIT </a> |
+                    <a href="confirm_delete.php?id=<?php echo $data['id']; ?>"> DELETE</a>
+                </td>
+            </tr>
+
+        <?php } ?>
+    </table>
+</body>
+
+</html>
